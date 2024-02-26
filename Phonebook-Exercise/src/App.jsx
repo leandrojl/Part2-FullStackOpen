4543eb7e47851f01c2cc3e187c454a8a0da24d9e
leandrojl <leandroracing96@hotmail.com>
2024-02-25 23:36:00 -0300

    Add person to phonebook

--- Phonebook-Exercise/src/App.jsx
+++ Phonebook-Exercise/src/App.jsx
@@ -1,34 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Arto Hellast' }
+  ]) 
+  const [newName, setNewName] = useState('new note')
+
+  const addPersonToThePhoneBook = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setNewName("")
+    setPersons(persons.concat(personObject))
+    console.log("form submitted")
+    console.log("new persons array", persons)
+  }
+
+  const handlePersonNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPersonToThePhoneBook}>
+        <div>
+          Name: <input onChange={handlePersonNameChange} value={newName}/>
+        </div>
+        
+        
+          <button type="submit">Save</button>
+        
+        
+      </form>
+      <div>debug: {newName}</div>
+      <h2>Added to Phonebook:</h2>
+      <ul>
+        {
+          persons.map(
+            person =>
+            <li key={person.name}>
+              Name: {person.name}
+            </li>
+            
+          )
+        }
+      </ul>
+      <h2>Numbers</h2>
+      
+    </div>
   )
 }
 
