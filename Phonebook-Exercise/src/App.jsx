09884bb426285e5174e573378570e8de86dbcee9
leandrojl <leandroracing96@hotmail.com>
2024-02-28 23:25:06 -0300

    Add number to persons array

--- Phonebook-Exercise/src/App.jsx
+++ Phonebook-Exercise/src/App.jsx
@@ -1,18 +1,39 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Arto Hellast' }
-  ]) 
+  const [persons, setPersons] = useState(
+    [
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ]
+  ) 
   const [newName, setNewName] = useState('new name')
+  const [newNumber, setNewNumber] = useState('new number')
+  const [newSearch, setNewSearch] = useState('search here...')
+
+  const handlePersonNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handlePersonNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSerchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
 
   const addPersonToThePhoneBook = (event) => {
     event.preventDefault()
-   
+   console.log(event.target.elements)
+   console.log(event.target.elements[0].value)
     
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     const checkPersonName = persons.some(person => person.name === newName) 
@@ -36,37 +57,60 @@ const App = () => {
     
   }
 
-  const handlePersonNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <p>Search your contact: <input onChange={handleSerchChange} value={newSearch} /> </p>
+      <div>debug newSearch: {newSearch}</div>
+      
+      <h2>Submit your contact:</h2>
       <form onSubmit={addPersonToThePhoneBook}>
-        <div>
-          Name: <input onChange={handlePersonNameChange} value={newName}/>
-        </div>
-        
+        <div>Name: <input onChange={handlePersonNameChange} value={newName}/></div>
+        <div>Number: <input onChange={handlePersonNumberChange} value={newNumber} /></div>
+       
         
           <button type="submit">Save</button>
         
         
       </form>
-      <div>debug: {newName}</div>
+      <div>debug newName: {newName}</div>
+      <div>debug newNumber: {newNumber}</div>
+      <h2>Filtered contacts</h2>
+      <ul>
+        {
+          persons.map(
+            person =>
+            <li key={person.name}>
+              <p>Name: {person.name}</p>
+              <p>Number: {person.number}</p>
+              
+              
+            </li>
+            
+            
+          )
+        }
+      </ul>
       <h2>Added to Phonebook:</h2>
       <ul>
         {
           persons.map(
             person =>
             <li key={person.name}>
-              Name: {person.name}
+              <p>Name: {person.name}</p>
+              <p>Number: {person.number}</p>
+              
+              
             </li>
+            
             
           )
         }
       </ul>
-      <h2>Numbers</h2>
+     
       
     </div>
   )
