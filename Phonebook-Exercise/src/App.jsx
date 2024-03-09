91b065942f74d24830fac18e04f8dc9a5c78b4f1
leandrojl <leandroracing96@hotmail.com>
2024-03-09 18:04:21 -0300

    Fetch data from db json with axios

--- Phonebook-Exercise/src/App.jsx
+++ Phonebook-Exercise/src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SearchFilter from "./components/SearchFilter"
 import PersonForm from "./components/PersonForm"
 import FilteredContacts from "./components/FilteredContacts"
 import Header from "./components/Header"
 import PersonsOnThePhoneBook from "./components/PersonsOnThePhoneBook"
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState(
-    [
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]
-  ) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('new name')
   const [newNumber, setNewNumber] = useState('new number')
   const [newSearch, setNewSearch] = useState('search here...')
   const [filteredArray, setFilteredArray] = useState([])
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
    
 
