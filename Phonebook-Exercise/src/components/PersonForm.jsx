852c18e087097ff4eb9a9cd99b0d4cb5e1eb7a87
leandrojl <leandroracing96@hotmail.com>
2024-03-31 14:25:14 -0300

    Fix ui form

--- Phonebook-Exercise/src/components/PersonForm.jsx
+++ Phonebook-Exercise/src/components/PersonForm.jsx
@@ -2,6 +2,8 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const PersonForm = (props) =>{
 
@@ -9,19 +11,34 @@ const PersonForm = (props) =>{
       
         <Form onSubmit={props.addPersonToThePhoneBook} >
           <Stack gap={2}>
+          
             <Form.Group controlId='formBasicName'>
-              <Form.Label>
-                Name:
-              </Form.Label>
-              <Form.Control  onChange={props.handlePersonNameChange} value={props.newName} placeholder='Enter name'/>
+            <Row>
+                <Col lg={4}>
+                  <Form.Label>
+                  Name:
+                  </Form.Label>
+                </Col>
+                <Col lg={8}>
+                <Form.Control  onChange={props.handlePersonNameChange} value={props.newName} placeholder='Enter name'/>
+                </Col>
+              </Row>
+              
             </Form.Group>
 
             <Form.Group controlId='formBasicNumber'>
-              <Form.Label>
-                Number:
-              </Form.Label>
-              <Form.Control onChange={props.handlePersonNumberChange} value={props.newNumber} placeholder='Enter number' />
-            </Form.Group>
+              <Row>
+                <Col lg={4}>
+                  <Form.Label>
+                  Number:
+                  </Form.Label>
+                </Col>
+                <Col lg={8}>
+                <Form.Control onChange={props.handlePersonNumberChange} value={props.newNumber} placeholder='Enter number' />
+                </Col>
+              </Row>
+              
+              </Form.Group>
             <Button className='mb-2 mt-1' variant='primary' type='submit'>Save</Button>
             </Stack>
           </Form>
